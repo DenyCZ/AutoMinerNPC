3f928dfd9ab87007075baf881067095a96503d16
Daniel Ciepluch <d.ciepluch@seznam.cz>
2019-11-26 18:41:19 +0100

    Add basic project structure

---
+++ src/main/java/cz/denyk/autominernpc/Main.java
@@ -0,0 +1,14 @@
+package cz.denyk.autominernpc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    public void onDisable() {
+
+    }
+}
